511430ddd3c69199b4fd11cbf7393c4722eda1df
Zerfilos <thi_kun@hotmail.com>
2022-06-21 19:07:00 -0300

    Configurando e aprendendo um pouco sobre graphcms e apollo

---
+++ src/lib/apollo.ts
@@ -0,0 +1,6 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+
+export const client = new ApolloClient({
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oauc3n1net01z24ivc91r2/master',
+    cache: new InMemoryCache()
+})
