2ddef7fd4bf5868d0308284336124efd31b1fc6c
Zerfilos <thi_kun@hotmail.com>
2022-06-21 21:58:49 -0300

    Feito o header e a parte visual do sidebar trazendo informação do graphcms

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-
-interface Lesson {
-  id: string,
-  title: string,
-}
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
+import { IgniteLab } from "./pages/IgniteLab"
 
 function App() {
-  // COMO É FEITO COM USEEFFECT
-  //
-  /* useEffect(() => {
-     client.query({
-       query: GET_LESSONS_QUERY,
-     }).then(response => {
-       console.log(response.data);
-     })
-   },[])
-   */
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY); //COMO É FEITO COM O USEQUERY DO APOLLO
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {  //? NO DATA POIS PODE RETORNAR COMO UNDEFINED
-        return (
-          <li key={lesson.id}>
-            {lesson.title}
-          </li>
-        )
-      })}
-    </ul>
+    <IgniteLab />
   )
 }
-
-/*
-COMENTARIO GIT
-
-tailwindcss
-graphql
-graphcms (headless cms)
-*/
 
 export default App
