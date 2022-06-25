0f41075b337939863847f047d0b006fe26aa57ef
Zerfilos <thi_kun@hotmail.com>
2022-06-24 22:24:59 -0300

    Parte 1 - Feito a interface da main page, falta fazer a criação de usuarios com graphcms

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import { IgniteLab } from "./pages/IgniteLab";
+import { Subscribe } from "./pages/Subscribe";
 
 export function Router() {
     return(
         <Routes>
-            <Route path="/" element={<h1>Home</h1>} />
+            <Route path="/" element={<Subscribe />} />
             <Route path="/ignite-lab" element={<IgniteLab />} />
             <Route path="/ignite-lab/lesson/:slug" element={<IgniteLab />} />
         </Routes>
