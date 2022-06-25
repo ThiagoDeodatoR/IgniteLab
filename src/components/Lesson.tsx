b17c16328c5714f9c3719ed64567217d75954729
Zerfilos <thi_kun@hotmail.com>
2022-06-25 17:24:26 -0300

    Parte 2 - Feito a implementação do submit na main page com o graphcms, css ajustado

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,6 @@
 import { CheckCircle, Lock } from 'phosphor-react';
 import { format, isPast } from 'date-fns'; //ISPAST VERIFICA SE A DATA JA PASSOU
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import ptBR from 'date-fns/locale/pt-BR';
 
 
@@ -13,10 +13,14 @@ interface LessonProps {
 }
 
 export function Lesson(props: LessonProps) {
+    const { slug } = useParams<{slug: string}>();
+
     const isLessonAvailable = isPast(props.availableAt);
     const availableAtDateFormated = format(props.availableAt, "EEEE' • 'd' de 'MMMM' • 'k'h'mm", {
         locale: ptBR,
-    }) 
+    }); 
+
+    const isLessonActive = slug === props.slug;
 
     return (
         <Link to={`/ignite-lab/lesson/${props.slug}`} className="group">
@@ -24,10 +28,10 @@ export function Lesson(props: LessonProps) {
                 {availableAtDateFormated.charAt(0).toLocaleUpperCase() + availableAtDateFormated.slice(1)}
             </span>
 
-            <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors">
+            <div className={`rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500 transition-colors ${isLessonActive && 'bg-green-500'}`}>
                 <header className="flex items-center justify-between">
                     {isLessonAvailable ? (
-                        <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
+                        <span className={`text-sm ${isLessonActive ? 'text-white' : 'text-blue-500'} font-medium flex items-center gap-2`}>
                             <CheckCircle size={20} />
                             Conteúdo liberado
                         </span>
@@ -38,12 +42,12 @@ export function Lesson(props: LessonProps) {
                         </span>
                     )}
 
-                    <span className="text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold">
+                    <span className={`text-xs rounded px-2 py-[0.125rem] text-white border ${isLessonActive ? 'border-white' : 'border-green-300'} font-bold`}>
                         {props.type === 'live' ? 'AO VIVO' : 'Aula Prática'}
                     </span>
                 </header>
 
-                <strong className="text-gray-200 mt-5 block">
+                <strong className={`${isLessonActive ? 'text-white' : 'text-gray-200'} mt-5 block`}>
                     {props.title}
                 </strong>
             </div>
